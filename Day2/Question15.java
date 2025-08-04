cf85843f0ed1be3a5e3514ef301f66b480a235cf
Kushal-Narendracumar7 <kushalbamania7@gmail.com>
2025-08-04 12:29:49 +0530

    Added minor changes

--- Day2/Question15.java
+++ Day2/Question15.java
@@ -2,9 +2,10 @@ public class Question15 {
     public static void main(String[] args) {
         // Default constructor
         CourseDetails course1 = new CourseDetails();
-
+        course1.display();
         // Parameterized constructor
         CourseDetails course2 = new CourseDetails(102, "Python Programming", "4 Months");
+        course2.display();
     }
 }
 
@@ -14,15 +15,16 @@ class CourseDetails{
     String cDuration = "3 Months";
 
     CourseDetails(){
-        System.out.println("Course ID: " + cId);
-        System.out.println("Course Name: " + cName);
-        System.out.println("Course Duration: " + cDuration);
+        
     }
 
     CourseDetails(int id, String name, String duration){
         this.cId = id;
         this.cName = name;
         this.cDuration = duration;
+        
+    }
+    void display(){
         System.out.println("Course ID: " + cId);
         System.out.println("Course Name: " + cName);
         System.out.println("Course Duration: " + cDuration);
